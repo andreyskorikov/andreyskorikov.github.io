0ba94eb07d18e0309d7fc76f0958189ce4d6634b
andreyskorikov <a.skorikov@insightwhale.com>
2021-07-04 03:24:21 +0600

    trnx added

--- js/product.js
+++ js/product.js
@@ -6,8 +6,11 @@ document.addEventListener("click", function(e) {
     var n; 
     var t = ~~(Date.now() / 1000)
     if (t >= 1627776000){n = 1;} else {n = 0;}
+	
+	var trnx = ga.getAll()[0].get('clientId') + "-" + t.toString();
   
     if (a) {
+		localStorage.transaction = trnx;
 		localStorage.product = a;
         localStorage.price = c[n].trim().split(" ")[0];
 		localStorage.currency = c[n].trim().split(" ")[1];
