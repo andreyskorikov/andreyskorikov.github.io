adf3766639f82c5cc1fe41f41c40604296853896
andreyskorikov <a.skorikov@insightwhale.com>
2022-04-04 09:15:18 +0600

    gtag added

---
+++ js/ecommerce_gtag.js
@@ -0,0 +1,18 @@
+gtag('event', 'purchase', {
+  'clientId' : '32364243.1648573681',             // unique id of a browser
+  'event_category' : 'ecommerce',
+  'event_action' : 'checkout',
+  'event_label' : 'success - main product',
+  "transaction_id": '167154',                      // order ID
+  "affiliation": 'Online Store',                   // Platform of purchase or affiliate
+  "value": '258.96',                               // product price of all 6 bottles
+  "currency": "USD",
+  "items": [
+    {
+      "id": 'YB-6PLUS3',                      // Product SKU is required.
+      "name":'6 Bottles of Youthful Brain',  // Name or ID is required.
+      "quantity": 6,
+      "price": '43.16',                       // product price of one bottle
+    }
+  ]
+});
---
+++ js/gtag.js
@@ -0,0 +1,7 @@
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){window.dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-197075322-1'{
+  'custom_map': {'dimension2': 'clientId'}
+});
