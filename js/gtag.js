022b5c1dec76619ba233b06785242845c5cceb40
andreyskorikov <a.skorikov@insightwhale.com>
2022-04-04 09:17:50 +0600

    Update gtag.js

--- js/gtag.js
+++ js/gtag.js
@@ -1,3 +1,6 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"></script>
+<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){window.dataLayer.push(arguments);}
   gtag('js', new Date());
@@ -5,3 +8,4 @@
   gtag('config', 'UA-197075322-1'{
   'custom_map': {'dimension2': 'clientId'}
 });
+</script>
